3c3064f13da5ac31be32b4fd1e2a8176e7929314
seong8681 <hazzyseong@gmail.com>
2019-03-21 23:48:31 +0900

    source Code

--- OperatingSystem/source/srtf.cpp
+++ OperatingSystem/source/srtf.cpp
@@ -10,12 +10,12 @@
 using namespace std;
 
 struct Process {
-    int processNum; // 프로세스 번호
-    int arraivedTime; // 도착시간
-    int usingTime; // CPU 사용시간
+    int processNum; // PID
+    int arraivedTime; // Time of arrival in Ready state
+    int usingTime; // CPU using time
 };
 
-int sec = 0 , cnt = 0;
+int sec = 0 , cnt = 0; // CPU running time, Number of processes in ready state
 
 bool compare1 (const Process& x, const Process& y) {
     if(x.arraivedTime == y.arraivedTime) {
@@ -39,18 +39,20 @@ bool compare2(const Process &x, const Process &y) {
 }
 
 vector<Process> readyQ;
-list<int> TIME;
+list<int> TIME; // Save the time the processes arrive
 
 int solution() {
-    int answer = 0, len;
-    vector<Process>::iterator cur = readyQ.begin(), next;
+    int answer = 0;
+    vector<Process>::iterator cur = readyQ.begin();
     while(!readyQ.empty()) {
+        /**terminated */
         if(cur->usingTime == 0) {
             cnt--;
             readyQ.erase(cur);
             if(readyQ.empty()) break;
         }
         if(sec == TIME.front()) {
+            /**ready */
             while(!TIME.empty()) {
                 if(sec == TIME.front()) {
                     cnt++;
@@ -59,8 +61,10 @@ int solution() {
                 else break;
             }
         }
+        /**Time interrupt & Scheduler dispatch */
         sort(readyQ.begin(), readyQ.end(), compare2);
         sec++;
+        /**Running */
         if(cnt > 0) {
             answer += cnt - 1;
             cur->usingTime--;
@@ -78,7 +82,7 @@ int main() {
 		exit(1);
 	}
 
-    int N; // 프로세스 개수
+    int N; // Number of process
     inp >> N;
     readyQ.reserve(N);
 
